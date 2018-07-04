3bc2ed36091da753ae71e79883b789c8f330a15d
mcanatalay <mcanatalay@hotmail.co.uk>
2018-07-04 03:14:25 +0300

    Last version

--- dependency_parser.py
+++ dependency_parser.py
@@ -1,172 +1,88 @@
 from data_utils import DataUtils
 import numpy as np
-from keras.engine import Model
-from keras.models import Sequential, load_model
-from keras.layers import Dense, Layer, RepeatVector, Masking, Concatenate, Add, Reshape, Activation, Dropout, LSTM, TimeDistributed, Bidirectional, Embedding, Input
+from keras.models import Sequential, load_model, Model
+from keras.layers import Dense, Activation, Embedding, LSTM, Input, Add, Concatenate
 from keras.utils import plot_model
 import keras.backend as K
-
-class BiLSTM(Layer):
-
-    def __init__(self, output_dim, **kwargs):
-        self.output_dim = output_dim
-        self.f_lstm = LSTM(output_dim, dropout=0.35, recurrent_dropout=0.1, return_sequences=True)
-        self.b_lstm = LSTM(output_dim, dropout=0.35, recurrent_dropout=0.1, return_sequences=True)
-        self.bilstm = Concatenate()
-        super(BiLSTM, self).__init__(**kwargs)
-
-    def build(self, input_shape):
-        super(BiLSTM, self).build(input_shape)  # Be sure to call this at the end
-
-    def call(self, x):
-        forward_output = self.f_lstm(x[0])
-        backward_output = self.b_lstm(x[1])
-        backward_output = K.reverse(backward_output, [1])
-        backward_output = Reshape((input_shape[0][1],self.output_dim))(backward_output)
-        bilstm_output = self.bilstm([forward_output, backward_output])
-        return bilstm_output
-
-    def compute_output_shape(self, input_shape):
-        return (input_shape[0][0], input_shape[0][1], self.output_dim*2)
 
 class DependencyParser(object):
     def __init__(self):
         pass
 
-    def __create_xy(self, dependency_tree, embedding_file, data_size, look_back, test=False):
-        sentences, words, tags = DataUtils.parse_dependency_tree(dependency_tree)
-        word_emb = DataUtils.load_embeddings(embedding_file)
-        tag_int = DataUtils.create_int_dict(tags)
+    def __create_xy(self, parse_tree_file, data_size, seq_len, test=False):
+        sentences, words, tags = DataUtils.parse_dependency_tree(parse_tree_file)
+        word_int = DataUtils.create_int_dict(words)
+        tag_int = DataUtils.create_onehot_vectors(tags)
 
-        data_size = int(len(sentences)*min(data_size, 1))
-
-        if test:
-            sentences.reverse()
-
-        if look_back == 0:
-            for sentence in sentences[:data_size]:
-                look_back = max(look_back, len(sentence))
-
-        self.look_back = look_back
+        self.seq_len = seq_len
         self.distinct_words = len(words)
         self.distinct_tags = len(tags)
 
-        word_full_forward = []
-        word_full_backward = []
-        word_instance_forward = []
-        word_instance_backward = []
+        data_len = 0
+        for i in range(len(sentences)):
+            data_len += int(np.ceil(len(sentences[i])/seq_len))*seq_len*seq_len
 
-        tag_full_forward = []
-        tag_full_backward = []
-        tag_instance_forward = []
-        tag_instance_backward = []
+        forward = np.zeros((2,data_len,seq_len,),dtype="int32")
+        backward = np.zeros((2,data_len,seq_len,),dtype="int32")
+        probability = np.zeros((data_len,),dtype="float32")
+        tags = np.zeros((data_len,18))
 
-        head = []
-
-        progress = 0
-
-        for sentence in sentences[:data_size]:
-            parts = [sentence[i:i+look_back] for i in range(0,len(sentence),look_back)]
+        idx = 0
+        for sentence in sentences:
+            parts = [sentence[i:i+seq_len] for i in range(0,len(sentence),seq_len)]
             for part in parts:
-                word_temp = np.zeros((2,look_back,300))
-                tag_temp = np.zeros((2,look_back,),dtype="int32")
+                part_len = len(part)
+                word_forward = np.zeros((seq_len,seq_len),dtype="int32")
+                word_backward = np.zeros((seq_len,seq_len),dtype="int32")
 
-                head_instance = np.zeros((look_back,1),dtype="float32")
+                for jdx in range(part_len):
+                    word_forward[jdx][seq_len-jdx-1:] = [word_int[part[i]["word"]] for i in range(jdx+1)]
+                    word_backward[jdx][seq_len-part_len+jdx:] = [word_int[part[part_len-i-1]["word"]] for i in range(part_len-jdx)]
 
-                for idx in range(len(part)):
-                    word = part[idx]["word"]
-                    word_temp[0][look_back-len(part)+idx] = word_emb[word] if word in word_emb else word_emb["UNK"]
-                    word_temp[1][look_back-idx-1] = word_emb[word] if word in word_emb else word_emb["UNK"]
+                for jdx in range(part_len):
+                    for zdx in range(part_len):
+                        tags[idx] = tag_int[part[jdx]["tag"]]
+                        forward[0][idx] = word_forward[jdx]
+                        forward[1][idx] = word_forward[zdx]
+                        backward[0][idx] = word_backward[jdx]
+                        backward[1][idx] = word_backward[zdx]
+                        probability[idx] = 1.0 if part[jdx]["head"] == part[zdx]["word"] else 0.0
+                        idx += 1
 
-                    tag = part[idx]["tag"]
-                    tag_temp[0][look_back-len(part)+idx] = tag_int[tag]
-                    tag_temp[1][look_back-idx-1] = tag_int[tag]
+                        if idx%int(data_len/100) == 0:
+                            DataUtils.update_message(str(int(idx/data_len*100)))
+        if test:
+            forward = [np.array(forward[0][5000:10000]),np.array(forward[1][5000:10000])]
+            backward = [np.array(backward[0][5000:10000]),np.array(backward[1][5000:10000])]
+            probability = np.array(probability[5000:10000])
+            tags = np.array(tags[5000:10000])
+        else:
+            forward = [np.array(forward[0][:5000]),np.array(forward[1][:5000])]
+            backward = [np.array(backward[0][:5000]),np.array(backward[1][:5000])]
+            probability = np.array(probability[:5000])
+            tags = np.array(tags[:5000])
 
-                word_instance = np.zeros((2,look_back,300))
-                tag_instance = np.zeros((2,look_back,),dtype="int32")
+        return [forward[0], backward[0], forward[1], backward[1]], [tags,probability]
 
-                for jdx in range(len(part)):
-                    word_instance[0][look_back-jdx-1:] = word_temp[0][look_back-len(part):look_back-len(part)+jdx+1]
-                    word_instance[1][look_back-len(part)+jdx:] = word_temp[1][look_back-len(part):look_back-jdx]
-
-                    tag_instance[0][look_back-jdx-1:] = tag_temp[0][look_back-len(part):look_back-len(part)+jdx+1]
-                    tag_instance[1][look_back-len(part)+jdx:] = tag_temp[1][look_back-len(part):look_back-jdx]
-
-                    head_instance = np.zeros((look_back,1), dtype="float32")
-
-                    for zdx in range(len(part)):
-                        head_instance[zdx] = 1 if part[jdx]["head"] == part[zdx]["word"] else 0
-                    if len(word_full_forward) == 0:
-                        word_full_forward = [word_temp[0]]
-                        word_full_backward = [word_temp[1]]
-                        word_instance_forward = [word_instance[0]]
-                        word_instance_backward = [word_instance[1]]
-
-                        tag_full_forward = [tag_temp[0]]
-                        tag_full_backward = [tag_temp[1]]
-                        tag_instance_forward = [tag_instance[0]]
-                        tag_instance_backward = [tag_instance[1]]
-
-                        head = [head_instance]
-                    else:
-                        word_full_forward = np.append(word_full_forward, [word_temp[0]], axis=0)
-                        word_full_backward = np.append(word_full_backward, [word_temp[1]], axis=0)
-                        word_instance_forward = np.append(word_instance_forward, [word_instance[0]], axis=0)
-                        word_instance_backward = np.append(word_instance_backward, [word_instance[1]], axis=0)
-
-                        tag_full_forward = np.append(tag_full_forward, [tag_temp[0]], axis=0)
-                        tag_full_backward = np.append(tag_full_backward, [tag_temp[1]], axis=0)
-                        tag_instance_forward = np.append(tag_instance_forward, [tag_instance[0]], axis=0)
-                        tag_instance_backward = np.append(tag_instance_backward, [tag_instance[1]], axis=0)
-
-                        head = np.append(head, [head_instance], axis=0)
-
-            DataUtils.update_message(str(progress)+"/"+str(data_size))
-            progress += 1
-
-        word_data = [(word_full_forward, word_full_backward), (word_instance_forward, word_instance_backward)]
-        tag_data = [(tag_full_forward, tag_full_backward), (tag_instance_forward, tag_instance_backward)]
-
-        return [word_data[0][0], word_data[0][1], tag_data[0][0], tag_data[0][1], word_data[1][0], word_data[1][1], tag_data[1][0], tag_data[1][1]], head
-
-    def create_xy_test(self, dependency_tree, embedding_file, data_size=1, look_back=0, mode="create", load=None):
-        DataUtils.message("Prepearing Test Data...", new=True)
-
-        if mode == "create" or mode == "save":
-            word_test, head_test, tag_test = self.__create_xy(dependency_tree, embedding_file, data_size, look_back, test=True)
-
-        if mode == "save":
-            DataUtils.save_array(DataUtils.get_filename("DP_W","TEST"+"_"+str(look_back)), word_test)
-            DataUtils.save_array(DataUtils.get_filename("DP_H","TEST"+"_"+str(look_back)), head_test)
-            DataUtils.save_array(DataUtils.get_filename("DP_T","TEST"+"_"+str(look_back)), tag_test)
-
-        if mode == "load" and load is not None:
-            word_test = DataUtils.load_array(load[0])
-            head_test = DataUtils.load_array(load[1])
-            tag_test = DataUtils.load_array(load[2])
-
-        self.word_test = np.array(word_test)
-        self.head_test = np.array(head_test)
-        self.tag_test = np.array(tag_test)
-
-    def create_xy_train(self, dependency_tree, embedding_file, data_size=1, look_back=0, mode="create", load=None):
+    def create_xy_train(self, parse_tree_file, data_size=1, seq_len=10):
         DataUtils.message("Prepearing Training Data...", new=True)
 
-        if mode == "create" or mode == "save":
-            train_x, train_y = self.__create_xy(dependency_tree, embedding_file, data_size, look_back, test=False)
+        x_train, y_train = self.__create_xy(parse_tree_file, data_size, seq_len)
 
-        self.train_x = train_x
-        self.train_y = train_y
+        self.x_train = x_train
+        self.y_train = y_train
+
+    def create_xy_test(self, parse_tree_file, data_size=1, seq_len=10):
+        DataUtils.message("Prepearing Validation Data...", new=True)
+
+        x_test, y_test = self.__create_xy(parse_tree_file, data_size, seq_len, test=True)
+
+        self.x_test = x_test
+        self.y_test = y_test
 
     def save(self, note=""):
         DataUtils.message("Saving Model...", new=True)
-        directory = "weights/"
-
-        DataUtils.create_dir(directory)
-
-        file = DataUtils.get_filename("DP", note)+".h5"
-
-        self.model.save(directory+file)
+        self.model.save(DataUtils.get_filename("DP", note)+".h5")
 
     def load(self, file):
         DataUtils.message("Loading Model...", new=True)
@@ -174,88 +90,77 @@ class DependencyParser(object):
 
     def plot(self, note=""):
         DataUtils.message("Ploting Model...", new=True)
-        directory = "plot/"
-
-        DataUtils.create_dir(directory)
-
-        file = DataUtils.get_filename("DP", note)+".png"
-
-        plot_model(self.model, to_file=directory+file, show_shapes=True, show_layer_names=False)
+        plot_model(self.model, to_file=DataUtils.get_filename("DP", note)+".png", show_shapes=True, show_layer_names=False)
 
     def create(self):
         DataUtils.message("Creating The Model...", new=True)
-        word_full_forward = Input(shape=(self.look_back,300))
-        word_full_backward = Input(shape=(self.look_back,300))
 
-        tag_full_forward_input = Input(shape=(self.look_back,))
-        tag_full_backward_input = Input(shape=(self.look_back,))
-        tag_emb = Embedding(self.distinct_tags, 30, input_length=self.look_back, trainable=True)
+        input_forward = Input(shape=(self.seq_len,))
+        input_backward = Input(shape=(self.seq_len,))
 
-        tag_full_forward = tag_emb(tag_full_forward_input)
-        tag_full_backward = tag_emb(tag_full_backward_input)
+        head_forward = Input(shape=(self.seq_len,))
+        head_backward = Input(shape=(self.seq_len,))
 
-        full_forward = Concatenate()([word_full_forward, tag_full_forward])
-        full_backward = Concatenate()([word_full_backward, tag_full_backward])
+        word_embedding = Embedding(self.distinct_words, 128, input_length=self.seq_len, trainable=True)
+        input_forward_embedding = word_embedding(input_forward)
+        input_backward_embedding = word_embedding(input_backward)
 
-        bilstm = BiLSTM(300)([full_forward, full_backward])
-        dense_output = TimeDistributed(Dense(600, activation="linear"))(bilstm)
+        head_forward_embedding = word_embedding(head_forward)
+        head_backward_embedding = word_embedding(head_backward)
 
+        lstm_forward = LSTM(128)
+        lstm_backward = LSTM(128)
 
-        word_instance_forward = Input(shape=(self.look_back,300))
-        word_instance_backward = Input(shape=(self.look_back,300))
+        input_forward_lstm = lstm_forward(input_forward_embedding)
+        input_backward_lstm = lstm_backward(input_backward_embedding)
+        input_lstm = Concatenate()([input_forward_lstm,input_backward_lstm])
 
-        tag_instance_forward_input = Input(shape=(self.look_back,))
-        tag_instance_backward_input = Input(shape=(self.look_back,))
+        head_forward_lstm = lstm_forward(head_forward_embedding)
+        head_backward_lstm = lstm_backward(head_backward_embedding)
+        head_lstm = Concatenate()([head_forward_lstm,head_backward_lstm])
 
-        tag_instance_forward = tag_emb(tag_instance_forward_input)
-        tag_instance_backward = tag_emb(tag_instance_backward_input)
+        tag_output = Dense(18, activation="softmax")(input_lstm)
 
-        instance_forward = Concatenate()([word_instance_forward, tag_instance_forward])
-        instance_backward = Concatenate()([word_instance_backward, tag_instance_backward])
+        input_hidden = Dense(100, activation=None)
+        input_forward_hidden = input_hidden(input_lstm)
 
-        f_ilstm = LSTM(300, dropout=0.35, recurrent_dropout=0.1)
-        b_ilstm = LSTM(300, dropout=0.35, recurrent_dropout=0.1)
+        head_hidden = Dense(100, activation=None)
+        head_forward_hidden = head_hidden(head_lstm)
 
-        forward_ioutput = f_ilstm(instance_forward)
-        backward_ioutput = b_ilstm(instance_backward)
-        bilstm_ioutput = Concatenate()([forward_ioutput, backward_ioutput])
+        sum_hidden = Add()([input_forward_hidden, head_forward_hidden])
+        tanh_hidden = Activation("tanh")(sum_hidden)
 
-        dense_ioutput = Dense(600, activation="linear")(bilstm_ioutput)
-        repeat_ioutput = RepeatVector(self.look_back)(dense_ioutput)
+        arc_output = Dense(1, activation=None)(tanh_hidden)
 
-        sum_output = Add()([dense_output,repeat_ioutput])
+        model = Model(inputs=[input_forward,input_backward,head_forward,head_backward],outputs=[tag_output,arc_output])
 
-        hidden = TimeDistributed(Dense(600, activation="tanh"))(sum_output)
-        output = TimeDistributed(Dense(1, activation="softmax"))(hidden)
+        def nll1(y_true, y_pred):
+            # keras.losses.binary_crossentropy give the mean
+            # over the last axis. we require the sum
+            return K.sum(K.binary_crossentropy(y_true, y_pred), axis=-1)
 
-        model = Model(inputs=[word_full_forward, word_full_backward, tag_full_forward_input, tag_full_backward_input, word_instance_forward, word_instance_backward, tag_instance_forward_input, tag_instance_backward_input], outputs=output)
-        model.compile(loss='binary_crossentropy', optimizer="adam", metrics=['accuracy'])
-
+        model.compile(loss=['categorical_crossentropy',nll1], optimizer="adam", metrics=['accuracy'])
         self.model = model
 
     def train(self, epochs, batch_size=32):
         DataUtils.message("Training...", new=True)
-        self.model.fit(self.train_x, self.train_y, epochs=epochs, batch_size=batch_size)
+        self.model.fit(self.x_train, self.y_train, epochs=epochs, batch_size=batch_size)
 
-    def validate(self, batch_size=16):
-        DataUtils.message("Validation...")
-        return self.model.evaluate([self.word_test,self.tag_test], self.head_test, batch_size=batch_size)
-
-    def predict(self, x):
-        return self.model.predict(x)
+    def validate(self, batch_size=32):
+        DataUtils.message("Validation...", new=True)
+        return self.model.evaluate(self.x_test, self.y_test)
 
     def summary(self):
         self.model.summary()
 
 if __name__ == "__main__":
-    train_file = "data/penn-treebank.conllx"
+    parse_tree_file = "data/penn-treebank.conllx"
     embedding_file = "embeddings/GoogleNews-vectors-negative300-SLIM.bin"
-    epochs = 10
-    look_back = 10 #0 means the largest window
 
     model = DependencyParser()
-    model.create_xy_train(train_file, embedding_file, 0.001, look_back = look_back)
+    model.create_xy_train(parse_tree_file, 1, 30)
+    model.create_xy_test(parse_tree_file, 1, 30)
     model.create()
-    model.summary()
-    model.train(epochs)
-    model.plot("TEST 2")
+    model.train(5)
+
+    DataUtils.message(model.validate())
